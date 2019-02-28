54fdf92574cec9920f84faf3caf5fc7f28f5d328
yuk7 <yukx00@gmail.com>
2019-02-28 13:19:32 +0900

    env.sh: remove launcher

--- env.sh
+++ env.sh
@@ -10,11 +10,6 @@ FRTCP_VER="1.23-1"
 FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
 FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
 
-LNCR_BLD="19022300"
-LNCR_ZIP="icons.zip"
-LNCR_FN="Arch.exe"
-LNCR_URL="https://github.com/yuk7/wsldl/releases/download/${LNCR_BLD}/${LNCR_ZIP}"
-
 PAC_PKGS="bzip2 coreutils diffutils gawk gcc-libs gettext grep gzip inetutils iproute2 iputils less man-db man-pages nano sed sudo tar vi vim wget which"
 
 if type curl >/dev/null 2>&1 ;then
